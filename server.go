94eae120bcd89bd371d5d8e14455f0b755d2eaf0
Ivan Davletshin <1374546+pruh@users.noreply.github.com>
2018-03-18 14:22:42 -0400

    add more request logs
    * add more request logs
    
    * add more request logs

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/gorilla/mux"
 	"github.com/pruh/api/controllers"
@@ -18,15 +20,26 @@ func main() {
 
 	apiV1Path := "/api/v1"
 
-	n := negroni.Classic()
 	router := mux.NewRouter().StrictSlash(false)
 	apiV1Router := mux.NewRouter().PathPrefix(apiV1Path).Subrouter()
-	router.PathPrefix(apiV1Path).Handler(negroni.New(
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+		negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+			requestDump, err := httputil.DumpRequest(r, true)
+			if err != nil {
+				log.Println(err)
+			}
+			log.Println(string(requestDump))
+
+			next(w, r)
+		}),
 		negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 			middleware.AuthMiddleware(w, r, next, config)
 		}),
 		negroni.Wrap(apiV1Router),
-	))
+	)
+	router.PathPrefix(apiV1Path).Handler(n)
 
 	tc := &controllers.TelegramController{
 		Config:     config,
@@ -36,7 +49,6 @@ func main() {
 
 	// n.Use(negroni.HandlerFunc(AuthMiddleware)) // global middleware
 
-	n.UseHandler(router)
-
-	n.Run(":" + *config.Port)
+	log.Printf("listening on :%s", *config.Port)
+	log.Fatal(http.ListenAndServe(":"+*config.Port, router))
 }
